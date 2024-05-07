e41455343a7f3e15259555375cf82bd72a87f42e
Austin Maddison <Austin.madison10152@gmail.com>
2024-05-07 15:30:24 +0700

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
     content: ["./build/**/*.{html,js}"],
     theme: {
+      screens: {
+        'xs': '475px',
+        // => @media (min-width: 475px) { ... }
+
+        'sm': '700px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '800px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+
+      },
       extend: {},
     },
     plugins: [],
